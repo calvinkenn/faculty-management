c0cc619ba2fd224debe31b55e0b8942b078c986c
calvinkenn <calvink.palma@gmail.com>
2022-04-21 17:06:32 +0800

    modal message fixed

--- backend/app.js
+++ backend/app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const usersRoutes = require("./routes/users-routes");
+const HttpError = require('./models/http-error');
 
 const app = express();
 //code for body parser
@@ -18,6 +19,19 @@ app.use((req, res ,next) => {
 });
 
 app.use('/api/users', usersRoutes);
+
+app.use((req, res, next) => {
+  const error = new HttpError('Could not find this route.', 404);
+  throw error;
+});
+
+app.use((error, req, res, next) => {
+  if (res.headerSent) {
+    return next(error);
+  }
+  res.status(error.code || 500);
+  res.json({ message: error.message || 'An unknown error occurred!' });
+});
 
 //connection to mongoose
 mongoose
